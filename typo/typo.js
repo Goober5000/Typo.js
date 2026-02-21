a0ba42f07e63b0b5f2d874e29711aca8b109b485
Goober5000 <ipw47@mailbox.org>
2026-02-21 01:14:44 -0500

    hash tweak

--- typo/typo.js
+++ typo/typo.js
@@ -61,9 +61,9 @@ var Typo;
             var hash = 5381 + (seed * 1000);
             for (var i = 0; i < str.length; i++) {
                 hash = ((hash << 5) + hash) + str.charCodeAt(i);
-                hash = hash & hash; // Convert to 32-bit integer
+                hash = hash >>> 0; // Convert to 32-bit unsigned integer
             }
-            return Math.abs(hash);
+            return hash;
         },
         
         /**
